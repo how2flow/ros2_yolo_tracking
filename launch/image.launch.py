c3b15cc66b84289489b6496c01a93cb5eadc97fe
Steve Jeong <how2soft@gmail.com>
2023-06-13 17:30:13 +0900

    improve cap launch file
    Signed-off-by: Steve Jeong <how2soft@gmail.com>

--- launch/image.launch.py
+++ launch/image.launch.py
@@ -7,30 +7,29 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    image_params = LaunchConfiguration(
-    'image_params',
-    default = os.path.join(
-        get_package_share_directory('yolo_tracking'),
-        'param',
-        'image_config.yaml'))
+    cap_yaml_path = os.path.join(get_package_share_directory('yolo_tracking'),
+                                 'param',
+                                 'cap_config.yaml')
+
+    cap_arg = DeclareLaunchArgument('cap_conf', default_value = cap_yaml_path,
+                                    description = 'Full path of param yaml file')
+
+    yt_img_pub_node = Node(
+        package = 'yolo_tracking',
+        executable = 'img_pub',
+        name = 'img_pub',
+        parameters = [{'cap_param': LaunchConfiguration('cap_conf')}],
+        output = 'screen')
+
+    yt_img_sub_node = Node(
+        package = 'yolo_tracking',
+        executable = 'img_sub',
+        name = 'img_sub',
+        parameters = [{'cap_param': LaunchConfiguration('cap_conf')}],
+        output = 'screen')
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'image_params',
-            default_value = image_params,
-            description = 'Full path of parameter file'),
-
-        Node(
-            package = 'yolo_tracking',
-            executable = 'img_pub',
-            name = 'img_pub',
-            parameters = [image_params],
-            output='screen'),
-
-        Node(
-            package = 'yolo_tracking',
-            executable = 'img_sub',
-            name = 'img_sub',
-            parameters = [image_params],
-            output = 'screen'),
-        ])
+        cap_arg,
+        yt_img_pub_node,
+        yt_img_sub_node
+    ])
